c709073854c75e30daebfc7490d6a03edd03d86f
Yair Tolentino <92339589+YahirMaster777@users.noreply.github.com>
2024-07-19 15:35:21 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+import uvicorn
 from fastapi import FastAPI
 from routes.personas import person
 from routes.users import user
@@ -6,15 +8,14 @@ from routes.roles import rol
 from routes.roles_user import roles_user
 from fastapi.middleware.cors import CORSMiddleware
 
-
-
 app = FastAPI()
 
 origins = [
     "http://localhost:8080",
     "http://127.0.0.1:8080",
 ]
-# configuracion del middleware para la insersion en la base
+
+# configuración del middleware para la inserción en la base
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -23,10 +24,13 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
-# añadimos las rutas de nuestras tablas a nuestra api
+# añadimos las rutas de nuestras tablas a nuestra API
 app.include_router(person, prefix="/api")
 app.include_router(user, prefix="/api")
 app.include_router(baby, prefix="/api")
 app.include_router(rol, prefix="/api")
 app.include_router(roles_user, prefix="/api")
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
